e1f33da2e0b5e1f6c1f1ffb21efa0558035d9181
Jeff Morris <77036455+JeffTheModder@users.noreply.github.com>
2024-01-02 01:22:31 -0600

    debug

--- skill-tree.js
+++ skill-tree.js
@@ -411,7 +411,9 @@ function loadSkillRecursive(treeSkill, parentSkill) {
             }
             newSkillPosition.add(parentSkill.position);
             // let tempSpacing = [300, 600, 800, 950, 1100, 1250, 1400, 1550];
+            console.log(skills, progression)
             newSkill = new Skill(newSkillPosition.x, newSkillPosition.y, skills[progression[0]], progression[1], parentSkill, selectedSkill.skillSpacing);
+            console.log(newSkill)
             // TEMP: ig
             // newSkill = new Skill(newSkillPosition.x, newSkillPosition.y, skills[progression], parentSkill, tempSpacing);
             treeSkills.push(newSkill);
